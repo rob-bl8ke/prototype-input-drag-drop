aefeca49dfe6a48be5ffe1c974eaac841884cd95
Rob Blake <rob.bl8ke@outlook.com>
2024-10-18 16:08:50 +0200

    Add project state using the singleton and pub-sub pattern

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,49 @@
+enum ProjectStatus {
+    Active, Finished
+}
+
+class Project {
+    constructor(public id: string, public title: string, public description: string, public people: number, public status: ProjectStatus) {
+
+    }
+}
+
+type Listener = (items: Project[]) => void;
+
+class ProjectState {
+    private listeners: Listener[] = [];
+    private projects: Project[] = [];
+
+    // Singleton pattern
+    private static instance: ProjectState;
+    private constructor() { }
+
+    static getInstance() {
+        if (this.instance) {
+            return this.instance;
+        }
+        this.instance = new ProjectState();
+        return this.instance;
+    }
+
+    addListener(listenerFn: Listener) {
+        this.listeners.push(listenerFn);
+    }
+
+    addProject(title: string, description: string, numOfPeople: number) {
+        const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active);
+        
+        this.projects.push(newProject);
+        for (const listenerFn of this.listeners) {
+            // Pass a copy of the array
+            listenerFn(this.projects.slice());
+            // listenerFn([...this.projects]);
+        }
+    }
+}
+
+const projectState = ProjectState.getInstance();
+
 // Validation
 interface Validatable {
     value: string | number;
@@ -50,6 +96,7 @@ class ProjectList {
     templateElement: HTMLTemplateElement;
     hostElement: HTMLDivElement;
     element: HTMLElement
+    assignedProjects: Project[] = [];
 
     constructor(private type: 'active' | 'finished') {
         this.templateElement = document.getElementById('project-list') as HTMLTemplateElement;
@@ -58,8 +105,23 @@ class ProjectList {
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild as HTMLElement;
         this.element.id = `${this.type}-projects`;
+
+        projectState.addListener((projects: Project[]) => {
+            this.assignedProjects = projects;
+            this.renderProjects();
+        });
+
         this.attach();
         this.renderContent();
+    }
+
+    private renderProjects() {
+        const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+        for (const item of this.assignedProjects) {
+            const listItem = document.createElement('li');
+            listItem.textContent = item.title;
+            listEl?.appendChild(listItem);
+        }
     }
 
     private renderContent() {
@@ -128,7 +190,7 @@ class ProjectInput {
         // A tuple is simply an array in JavaScript
         if (Array.isArray(userInput)) {
             const [title, desc, people] = userInput;
-            console.log(title, desc, people);
+            projectState.addProject(title, desc, people);
             this.clearInputs();
         }
     }
