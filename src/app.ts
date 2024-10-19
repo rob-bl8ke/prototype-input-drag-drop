5d4438ab0ec71cb0d1a354085044e097d528ebbb
Rob Blake <rob.bl8ke@outlook.com>
2024-10-19 15:03:27 +0200

    Third party libraries: Use declare to tell typescript that a global is there that it cannot detect. Use as last resort.

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
 import _ from 'lodash';
 
+declare var GLOBAL: string;
+
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
 
 console.log(_.shuffle([1,2,3,4]));
+console.log(GLOBAL);
